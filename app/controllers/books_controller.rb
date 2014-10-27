000d48013912bf4b8bfac4fbfafbaea0c0b5d627
calvinchu8172 <calvinchu8172@gmail.com>
2014-10-27 09:50:38 +0800

    re-commit everything

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+
+before_action :set_book, :only => [ :show, :edit, :update, :destroy]
+
+def index
+	 # @books = Book.all
+	 @books = Book.page(params[:page]).per(5)
+end
+
+def new
+ 	@book = Book.new
+end
+
+def create
+  @book = Book.new(book_params)
+  if @book.save
+    redirect_to :action => :index
+    flash[:notice] = "Book was successfully created"
+  else
+	render :action => :new
+  end
+end
+
+def show
+  @page_title = @book.name
+end
+
+def edit
+  
+end
+
+def update
+  if @book.update_attributes(book_params)
+     redirect_to :action => :show, :id => @book
+     flash[:notice] = "Book was successfully updated"
+  else
+  	render :action => :edit
+  end
+end
+
+def destroy
+  @book.destroy
+  redirect_to :action => :index
+  flash[:alert] = "Book was successfully deleted"
+end
+
+private
+
+def set_book
+  @book = Book.find(params[:id])
+end
+
+def book_params
+	params.require(:book).permit(:name, :description, :isbn)
+end
+
+
+end
---
+++ test/models/gook_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
